066f493f88f76c1d0fb3182afde90ee8dc820718
Dzulfiqar Amien <dzulfiamien@gmail.com>
2021-01-06 15:57:22 +0700

    intial commit

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-transparent navbar-expand-lg fixed-top bg-dark" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+            <img class="navbar-brand__regular" src="assets/img/brand-logo.png" alt="brand-logo">
+        </a>
+        <button data-toggle="collapse" data-target="#navbarResponsive"
+            class="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="nav navbar-nav ml-auto text-uppercase">
+                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger"
+                        href="#services">Services</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger"
+                        href="#portfolio">Portfolio</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger"
+                        href="#about">About</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" 
+                    href="#team">Team</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger"
+                        href="#contact">Contact</a></li>
+            </ul>
+        </div>
+        <div class="d-flex align-items-center ml-lg-1 ml-xl-2 mr-5 mr-sm-6 m-lg-0">
+            <div>
+                <a href="inner-login.html" class="btn btn-size--lg btn-hover--3d btn-bg--cta--4" style="background-color: #FED136;"><span class="btn__text">Login</span></a>
+            </div>
+        </div>
+    </div>
+</nav>
